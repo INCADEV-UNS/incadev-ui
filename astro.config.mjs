b21ec3b94d36b68aee60c848c193afe5b4a24b19
Nilton Ramos <niltonencarnacion17@gmail.com>
2025-10-29 11:42:02 -0500

    build: configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/',
+    site: "https://instituto.cetivirgendelapuerta.com",
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://127.0.0.1:8000',
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path,
+                },
+            },
+        },
+    },
+});
