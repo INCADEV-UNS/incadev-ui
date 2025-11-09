26756487e763742bfa845186ab88656525bcae52
NiltonRamosE <150812910+NiltonRamosE@users.noreply.github.com>
2025-11-09 16:55:33 -0500

    Update academic-config.ts documentation
    Removed redundant description about config object.

--- src/config/academic-config.ts
+++ src/config/academic-config.ts
@@ -1,7 +1,6 @@
 /**
  * @abstract Config file
  * @description Este archivo contiene la configuracion de la aplicacion.
- * para esto, se ha creado un objeto config que contiene la url de la api y los endpoints.
  **/
 
 export const config = {
